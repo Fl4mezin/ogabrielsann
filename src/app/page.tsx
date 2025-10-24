5a84a407a4e4037334eb762eb3262ae436841e32
Gabriel Santos <ogabrielsann@gmail.com>
2025-10-24 16:00:53 +0000

    mova a sessao de prova social para baixo da sessao da oferta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <HeroSection />
       <PainsSection />
       <SolutionsSection />
-      <TestimonialsSection />
       <OfferSection />
+      <TestimonialsSection />
       <GuaranteeSection />
       <Footer />
       <SocialProofNotification />
